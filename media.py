15d18a522c0280c609240e9f3efb6c122f4e6a32
Karen Hou <ih2g10@gmail.com>
2017-08-01 09:13:24 +0900

    Fix indent issue that cause media.py fail to compile

--- media.py
+++ media.py
@@ -5,9 +5,8 @@ import webbrowser
 
 class Movie():
 
-	'''Intiate objects'''
-    def __init__(self, movie_title, movie_storyline, poster_image,
-                 trailer_youtube):
+    '''Intiate objects'''
+    def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
